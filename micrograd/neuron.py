5564b4d2b51ba9de1be8420f80963a55b6b575f4
kl1nokas <145532793+kl1nokas@users.noreply.github.com>
2026-04-03 12:47:19 +0500

    Update neuron.py

--- micrograd/neuron.py
+++ micrograd/neuron.py
@@ -1,22 +1,45 @@
+import numpy as np
 from engine import Tensor
-import numpy
 import random
 
 class Module:
-
     def zero_grad(self):
-        for p in self.parametrs:
-            p.grad = 0
+        for p in self.parametrs(): 
+            p.grad = np.zeros_like(p.data)
 
     def parametrs(self):
         return []
-    
-class Neuron():
 
-    def __int__(self, nin, nonlin=True):
-        self.w = [Tensor(random.uniform(-1, 1)) for _ in range(nin)]
-        self.b = Tensor(0)
-        self.nonlin = nonlin
+
+class Linear(Module):
+    def __init__(self, nin, nout):
+        self.W = Tensor(np.random.randn(nout, nin))
+        self.b = Tensor(np.zeros((nout, )))
 
     def __call__(self, x):
-        act = sum((wi*xi for i in zip(self.w, x)), self.b)
+        return self.W * x + self.b
+
+    def parametrs(self):
+        return [self.W, self.b]
+
+
+class MLP(Module):
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [
+            Linear(sz[i], sz[i+1])  
+            for i in range(len(nouts))
+        ]
+
+    def __call__(self, x):
+        if not isinstance(x, Tensor):
+            x = Tensor(x)
+
+        for i, layer in enumerate(self.layers):
+            x = layer(x)  
+            if i != len(self.layers) - 1:
+                x = x.relu()  
+        return x
+
+    def parametrs(self):
+        return [p for l in self.layers for p in l.parametrs()]
